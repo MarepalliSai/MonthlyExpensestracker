d73796a672d66cdd64bf9dc72871a62508a64e97
Sai Prasanna Marepalli <153057867+MarepalliSai@users.noreply.github.com>
2024-02-07 13:52:52 +0530

    Delete MonthlyExpensestracker-main/MonthlyExpenses directory

--- MonthlyExpensestracker-main/MonthlyExpenses/MonthlyExpenses/src/main/java/com/ecentum/MonthlyExpenses/exceptions/ExpenseTypeAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.ecentum.MonthlyExpenses.exceptions;
-
-public class ExpenseTypeAlreadyExistsException extends Exception {
-    public ExpenseTypeAlreadyExistsException(String message) {
-        super(message);
-    }
-
-}
